bd32626c8297b7989f34af5d04ac401c99e23acc
Guilherme <guilhermeviegas1993@gmail.com>
2025-06-08 23:23:19 +0200

    try2

--- react_portfolio/src/index.jsx
+++ react_portfolio/src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css'
+import 'ol/ol.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
