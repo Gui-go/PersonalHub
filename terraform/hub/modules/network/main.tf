674ea72da3d0ba1a1abecc2bbaec3649e7c4c762
Guilherme <guilhermeviegas1993@gmail.com>
2025-06-15 13:12:07 +0200

    improved networking

--- terraform/hub/modules/network/main.tf
+++ terraform/hub/modules/network/main.tf
@@ -17,6 +17,24 @@ resource "google_compute_subnetwork" "vpc_subnet" {
   private_ip_google_access = true
 }
 
+resource "google_compute_router" "nat_router" {
+  name    = "personalhub-router"
+  network = google_compute_network.vpc_net.name
+  region  = var.region
+}
+
+resource "google_compute_router_nat" "nat_config" {
+  name                               = "personalhub-nat"
+  router                             = google_compute_router.nat_router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
+
 resource "google_vpc_access_connector" "run_connector" {
   project        = var.proj_id
   name           = "cloudrun-connector"
