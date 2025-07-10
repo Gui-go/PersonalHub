f9ab7a8cc366eced5e5201610a8685d871b2f609
Guilherme <guilhermeviegas1993@gmail.com>
2025-07-10 03:25:19 +0200

    hard coded proj

--- portfolio_app/src/pages/api/fetchPlusOne.ts
+++ portfolio_app/src/pages/api/fetchPlusOne.ts
@@ -2,7 +2,8 @@ import { initializeApp, getApps, cert, ServiceAccount } from 'firebase-admin/app
 import { getFirestore } from 'firebase-admin/firestore';
 
 const serviceAccount: ServiceAccount = {
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  // projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  projectId: "personalhub14",
   privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
 };
