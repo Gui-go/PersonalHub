e3374eb2cb6dbb4416c3d4a6fd2cacda2068da07
guigo <guilhermeviegas1993@gmail.com>
2025-09-02 22:52:20 +0200

    added app_run

---
+++ terragrunt/modules/app_run/main.tf
@@ -0,0 +1,32 @@
+resource "google_cloud_run_v2_service" "run_portfolio" {
+  project  = var.project_id
+  name     = var.app_name
+  location = var.region
+  ingress  = var.ingress
+  template {
+    containers {
+      image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.repo_name}/${var.app_name}:latest"
+      ports { container_port = var.port }
+      resources {
+        limits = {
+          cpu    = var.cpu # "2"  # "1"
+          memory = var.memory # "1024Mi" #"2048Mi" # "1024Mi" # "512Mi"
+        }
+      }
+    }
+    scaling {
+      max_instance_count = var.max_instance # 1 # 1
+      min_instance_count = var.min_instance # 0 
+    }
+    vpc_access {
+      connector = var.run_connector
+      egress = var.egress
+    }
+    timeout = var.timeout
+    service_account = var.service_account
+  }
+  traffic {
+    percent = 100
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+  }
+}
---
+++ terragrunt/modules/app_run/variables.tf
@@ -0,0 +1,73 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "Region of the resources"
+  type        = string
+}
+
+variable "app_name" {
+  description = "Name of the Cloud Run app"
+  type        = string
+}
+
+variable "repo_name" {
+  description = "Name of the Artifact Registry"
+  type        = string
+}
+
+variable "ingress" {
+  description = ""
+  type        = string
+}
+
+variable "egress" {
+  description = ""
+  type        = string
+}
+
+variable "port" {
+  description = ""
+  type        = number
+  default     = 8080
+}
+
+variable "cpu" {
+  description = "The number of CPUs"
+  type        = string
+}
+
+variable "memory" {
+  description = ""
+  type        = string
+}
+
+variable "min_instance" {
+  description = ""
+  type        = number
+  default     = 0
+}
+
+variable "max_instance" {
+  description = ""
+  type        = number
+  default     = 1
+}
+
+variable "run_connector" {
+  description = ""
+  type        = string
+}
+
+variable "timeout" {
+  description = ""
+  type        = string
+}
+
+variable "service_account" {
+  description = ""
+  type        = string
+}
+
